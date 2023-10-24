eadbc57b5031c4844dadbc571b540c54343ee544
Guigon14 <guilherme.gsantos@sptech.school>
2023-10-24 15:20:44 -0300

    Arrumando algumas coisas do site

--- Site/public/js/funcoes.js
+++ Site/public/js/funcoes.js
@@ -34,7 +34,7 @@ function finalizarAguardar(texto) {
     var divAguardar = document.getElementById("div_aguardar");
     divAguardar.style.display = "none";
 
-    var divErrosLogin = document.getElementById("div_erros_login");
+    var divErrosLogin = document.getElementById("cardErro");
     if (texto) {
         divErrosLogin.style.display = "flex";
         divErrosLogin.innerHTML = texto;
